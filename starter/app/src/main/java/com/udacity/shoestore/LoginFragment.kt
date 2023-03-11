969d549d60fd9283a66e681b50a25a6b6ec54b5e
Karla Pedraza <kpedraza@fb.com>
2023-03-11 18:21:11 +0000

    [ShoeStore] Add welcomeFragment

--- starter/app/src/main/java/com/udacity/shoestore/LoginFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/LoginFragment.kt
@@ -4,18 +4,39 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.udacity.shoestore.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
+    lateinit var binding: FragmentLoginBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding = DataBindingUtil.inflate<FragmentLoginBinding>(
+        binding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_login, container, false
         )
+        binding.buttonLogin.setOnClickListener { view ->
+            maybeNavigateToWelcomeFragment(view)
+        }
+        binding.buttonSignUp.setOnClickListener { view ->
+            maybeNavigateToWelcomeFragment(view)
+        }
         return binding.root
+    }
+
+    private fun maybeNavigateToWelcomeFragment(view: View) {
+        if ((binding.editTextEmail.text.toString() == "") || (binding.editTextPassword.text.toString() == "")) {
+            val myToast =
+                Toast.makeText(context, R.string.login_error_toast, Toast.LENGTH_SHORT)
+            myToast.show()
+            return
+        }
+        view.findNavController()
+            .navigate(LoginFragmentDirections.actionLoginFragmentToWelcomeFragment())
+
     }
 }
